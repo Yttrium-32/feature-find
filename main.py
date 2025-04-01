841d22386c740e18b4bd1074a8cb5a2a3ae6db4b
Shresth Prasad <shresthprasad7@gmail.com>
2025-04-01 17:36:28 +0530

    add basic functionality to webapp

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from modules.colordescriptor import ColorDescriptor
 from modules.searcher import Searcher
+from image_matcher.settings import STATIC_URL
 
 import argparse
 import cv2
 
 import os
 import time
-
-INDEX_PATH: str = "index.csv"
 
 def main() -> None:
     ap = argparse.ArgumentParser()
@@ -18,12 +17,6 @@ def main() -> None:
             help="Path to query image"
     )
 
-    ap.add_argument(
-            "-p", "--photos",
-            required=True,
-            help="Path to photos to match"
-    )
-
     args = vars(ap.parse_args())
 
     clr_dsc = ColorDescriptor((8, 12, 3))
@@ -31,12 +24,12 @@ def main() -> None:
     query = cv2.imread(args["query"])
     features = clr_dsc.describe(query)
 
-    searcher = Searcher(args["photos"], INDEX_PATH, clr_dsc)
+    searcher = Searcher(STATIC_URL, STATIC_URL + "index.csv", clr_dsc)
     results = searcher.search(features)
 
     icat = "kitty +kitten icat"
     for (_, result_id) in results:
-        os.system(f"{icat} photos/{result_id}")
+        os.system(f"{icat} {STATIC_URL}{result_id}")
         time.sleep(1)
 
 if __name__ == "__main__":
--- search_gui/views.py
+++ search_gui/views.py
@@ -1,5 +1,46 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import cv2
 
-def search_gui(request):
-    return HttpResponse("Hello World")
+from django.shortcuts import render
+from django.http import HttpRequest
+from django.core.files.storage import FileSystemStorage
+
+from modules.searcher import Searcher
+from modules.colordescriptor import ColorDescriptor
+
+from image_matcher.settings import BASE_DIR, MEDIA_ROOT, MEDIA_URL
+
+def search_gui(request: HttpRequest):
+    uploaded_image = None
+    matched_photos = []
+
+    if request.method == "POST" and request.FILES.get("image"):
+        image = request.FILES["image"]
+        fs = FileSystemStorage(location=MEDIA_ROOT / 'user_uploads')
+        filename = fs.save(image.name, image)
+        uploaded_image = MEDIA_URL + 'user_uploads/' + filename
+
+        saved_image_path = fs.path(filename)
+        query = cv2.imread(saved_image_path)
+
+        clr_dsc = ColorDescriptor((8, 12, 3))
+        searcher = Searcher(
+                MEDIA_ROOT.__str__(),
+                BASE_DIR / "index.csv",
+                clr_dsc
+        )
+
+        features = clr_dsc.describe(query)
+        results = searcher.search(features)
+
+        print(f"DEBUG: {BASE_DIR / 'index.csv'}")
+        print(f"DEBUG: {results=}")
+        for (_, result_id) in results:
+            print(f"DEBUG: {result_id=}")
+            matched_photos.append(f"{MEDIA_URL}{result_id}")
+
+        print(f"DEBUG: {matched_photos=}")
+
+    return render(request, 'index.html', {
+        'uploaded_image_url': uploaded_image,
+        'result_images': matched_photos
+    })
