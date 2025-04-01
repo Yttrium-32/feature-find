24bd3251040c48ac8b7754f3355aaf968fb070bb
Shresth Prasad <shresthprasad7@gmail.com>
2025-04-01 13:19:31 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from modules.colordescriptor import ColorDescriptor
+import argparse
+import glob
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument(
+        "-d", "--dataset",
+        required=True,
+        help = "Path to the directory that contains the images to be indexed"
+)
+
+ap.add_argument(
+        "-i", "--index",
+        required=True,
+        help = "Path to where the computed index will be stored"
+)
+
+args = vars(ap.parse_args())
+
+cd = ColorDescriptor((8, 12, 3))
+
+print(f"{args['dataset']=}")
+
+with open(args["index"], "w") as index_file:
+    for image_path in glob.glob(args["dataset"] + "/*.jpg"):
+        print(f"{image_path=}")
+        image_name = image_path.split("/")[-1]
+        image = cv2.imread(image_path)
+
+        features = cd.describe(image)
+
+        features = [str(f) for f in features]
+        index_file.write(f"{image_name},{','.join(features)}")
