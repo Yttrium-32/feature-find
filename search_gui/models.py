59a92355edd5e5587859bd2c1e873797f1cd80ab
Shresth Prasad <shresthprasad7@gmail.com>
2025-04-02 10:57:56 +0530

    add structure to db

--- search_gui/models.py
+++ search_gui/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class ImageFeatures(models.Model):
+    image_id = models.AutoField(primary_key=True)
+    image_hash = models.CharField(max_length=64, unique=True)
+    image_name = models.CharField(max_length=255)
+    features = models.JSONField()
